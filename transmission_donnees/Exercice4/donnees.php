fb2459b789d88f0df1c9437214762a2959b2225a
Marie Gandrille <mgandrille@yahoo.fr>
2020-04-24 16:38:23 +0200

    Add Exercises

---
+++ transmission_donnees/Exercice4/donnees.php
@@ -0,0 +1,31 @@
+<?php 
+
+$movies = [
+    [
+        "id" => 1, 
+        "title" => "Star Wars",
+        "director" => "Georges Lucas",
+        "category" => "Fantasy"
+    ],
+    [
+        "id" => 2, 
+        "title" => "Pulp Fiction",
+        "director" => "Quentin Tarantino",
+        "category" => "Action"
+    ],
+    [
+        "id" => 3, 
+        "title" => "Fight Club",
+        "director" => "David Fincher",
+        "category" => "Thriller"
+    ],
+    [
+        "id" => 4, 
+        "title" => "Forrest Gump",
+        "director" => "Robert Zemeckis",
+        "category" => "Drame"
+    ]
+]
+
+
+?>
---
+++ transmission_donnees/Exercice4/film.php
@@ -0,0 +1,48 @@
+<?php include('donnees.php') ; ?>
+<?php 
+$colonne = array_column($movies, 'id');
+$keyMovie = array_search($_GET['id'], $colonne); 
+$movieFound =  $movies[$keyMovie];
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body>
+        <h1>Film</h1>
+        <p>Vous avez demandé le film n° : <?= $_GET['id'] ; ?></p>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header">
+                            <h1><?= $movieFound["title"] ?></h1>
+                        </div>
+                        <div class="card-body">
+                            <p>Réalisateur : <?= $movieFound["director"] ?></p>
+                            <p>Catégorie : <?= $movieFound["category"] ?></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>
