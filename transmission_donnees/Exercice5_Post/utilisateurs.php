16c93d0f27368e64b287a954d48f4e1808336820
Marie Gandrille <mgandrille@yahoo.fr>
2020-04-27 13:54:19 +0200

    Add Exercises

---
+++ transmission_donnees/Exercice5_Post/utilisateurs.php
@@ -0,0 +1,29 @@
+<?php
+
+$connection = [
+    [
+        'mail' => 'client@client.com',
+        'password' => '12345azerty',
+        'role' => 'client'
+    ],
+    [
+        'mail' => 'johndoe@client.com',
+        'password' => 'johnDoe123',
+        'role' => 'client'
+    ],
+    [
+        'mail' => 'vendeur@vendeur.com',
+        'password' => 'az3rty',
+        'role' => 'vendeur'
+    ],
+    [
+        'mail' => 'admin@admin.com',
+        'password' => 'super@dmin',
+        'role' => 'admin'
+    ]
+];
+
+
+
+
+?>
